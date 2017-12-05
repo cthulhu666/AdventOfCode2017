84f6ecc1aff04f3e0d309cd248566d1b5b57665c
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-05 19:37:57 +0100

    Day 4 part II

--- day4.rb
+++ day4.rb
@@ -1,5 +1,15 @@
 def valid?(password)
-  password.size == password.uniq.size
+  password.size == without_anagrams(password.uniq).size
+end
+
+def without_anagrams(words)
+  words.reject do |word|
+    (words - [word]).any? { |w| anagram?(w, word) }
+  end
+end
+
+def anagram?(w1, w2)
+  w1.chars.permutation.include?(w2.chars)
 end
 
 data = File.new("day4.txt").readlines().map { |s| s.split(" ").map(&:strip) }
