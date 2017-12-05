4a176e242b6174169d5dea30ef562091dd41a2ab
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-05 20:40:26 +0100

    Day 2 part II

--- day2.rb
+++ day2.rb
@@ -1,5 +1,5 @@
-def checksum(rows)
-  rows.reduce(0, &method(:row_diff))
+def checksum(rows, f)
+  rows.reduce(0, &f)
 end
 
 def row_diff(sum, row)
@@ -7,5 +7,12 @@ def row_diff(sum, row)
   sum + (sorted[0] - sorted[-1]).abs
 end
 
+def evenly_divisible(sum, row)
+  pair = row.permutation(2).find { |arr| arr.first % arr.last == 0 }.sort
+  sum + pair.last / pair.first
+end
+
 data = File.new("day2.txt").readlines().map { |s| s.split(" ").map(&:strip).map(&:to_i) }
-puts checksum(data)
+
+puts checksum(data, method(:row_diff))
+puts checksum(data, method(:evenly_divisible))
