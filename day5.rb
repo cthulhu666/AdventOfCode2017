8928fe84364ce4a87a3022c1d603fcac52155309
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-05 19:24:10 +0100

    Day 5 part II

--- day5.rb
+++ day5.rb
@@ -1,18 +1,19 @@
-# NOTE: run with `RUBY_THREAD_VM_STACK_SIZE=50000000`
+# NOTE: run with `RUBY_THREAD_VM_STACK_SIZE=5000000000`
 
 def walk(data, pos, count)
   offset = data[pos]
   return count if pos + offset >= data.size
-  walk(inc(data, pos), pos + offset, count + 1)
+  walk(inc(data, pos, offset), pos + offset, count + 1)
 end
 
-def inc(data, pos)
+def inc(data, pos, offset)
   data.tap do |d|
-    d[pos] += 1
+    offset >= 3 ? d[pos] -= 1 : d[pos] += 1
   end
 end
 
 
 data = File.new("day5.txt").readlines().map(&:strip).map(&:to_i)
 
+# data = [0, 3, 0, 1, -3]
 puts walk(data, 0, 1)
