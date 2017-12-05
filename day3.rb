ee37379d71c3809d9fb9e09609d67f3f6c2aace5
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-05 18:01:31 +0100

    Day 3

---
+++ day3.rb
@@ -0,0 +1,21 @@
+def corners(d)
+  n = (1 + 2*d)
+  max = n * n
+  3.times.reduce([max]) do |a, _|
+    a << a.last - (n - 1)
+  end
+end
+
+# That's not a general solution :P
+
+N = 361527
+r = (N**0.5 / 2.0).floor
+
+corners = corners(r)
+
+mid = (corners[0] + r)
+
+y = N - mid
+x = r
+
+puts x + y
