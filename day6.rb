05784993c5e1436f86d2ffa7db4630e3ae0fd4ca
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-06 13:19:09 +0100

    Day 6 part II

--- day6.rb
+++ day6.rb
@@ -1,11 +1,18 @@
 # NOTE: run with `RUBY_THREAD_VM_STACK_SIZE=5000000000`
 
-BANKS = "14	0	15 12	11 11	3	5	1	6	8	4	9	1	8	4".split.map(&:to_i).freeze
+BANKS = "14\t0\t15\t12\t11\t11\t3\t5\t1\t6\t8\t4\t9\t1\t8\t4".split.map(&:to_i).freeze
 
 def redistribute(banks, count, history)
   return count if history.include?(banks)
   n, idx = most_blocks(banks)
   redistribute(move(banks.dup, n, idx), count + 1, history + [banks])
+end
+
+def redistribute2(banks, count, history)
+  return history if history.any? { |_k, v| v > 2 }
+  n, idx = most_blocks(banks)
+  redistribute2(move(banks.dup, n, idx), count + 1,
+                append_history(history, banks))
 end
 
 def move(banks, n, idx)
@@ -29,7 +36,16 @@ end
 
 def next_idx(len, idx)
   return idx + 1 if idx + 1 < len
-  return 0
+  0
+end
+
+def append_history(history, banks)
+  history[banks] ||= 0
+  history[banks] += 1
+  history
 end
 
 puts redistribute(BANKS, 0, [])
+
+h = redistribute2(BANKS, 0, {})
+puts h.count { |_k, v| v > 1 }
