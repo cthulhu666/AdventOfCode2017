b44798c952bf19043a1a89a3e32914c52f2e0df4
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-12 18:56:43 -0500

    Day 10 part II

--- day10.rb
+++ day10.rb
@@ -1,5 +1,5 @@
 def run(l, m, i = 0, skip = 0)
-  return l if m.empty?
+  return [l, i, skip] if m.empty?
   len = m.first
   run(reverse(l, i, len), m.drop(1), (i + len + skip) % 256, skip + 1)
 end
@@ -10,8 +10,27 @@ def reverse(list, i, len)
   reversed.cycle.lazy.drop(list.length - i).take(list.length).to_a
 end
 
-INPUT = [147, 37, 249, 1, 31, 2, 226, 0, 161, 71, 254, 243, 183, 255, 30, 70].freeze
+INPUT = "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70".freeze
+
+SUFFIX = [17, 31, 73, 47, 23].freeze
+
+def part1(input)
+  run(0.upto(255).to_a, input).first.take(2).reduce(:*)
+end
+
+def part2(input)
+  i, skip = 0, 0
+  l = 0.upto(255).to_a
+  64.times do
+    l, i, skip = run(l, input.dup, i, skip)
+  end
+  dense = l.each_slice(16).map do |e|
+    e.reduce(:^)
+  end
+  dense.map { |e| format('%.2x', e) }.join
+end
 
 if __FILE__ == $PROGRAM_NAME
-  p run(0.upto(255).to_a, INPUT).take(2).reduce(:*)
+  p part1(INPUT.split(',').map(&:to_i))
+  p part2(INPUT.bytes + SUFFIX)
 end
