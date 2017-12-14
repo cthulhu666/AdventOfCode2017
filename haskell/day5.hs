f35a893e201c18e9fc3ba1f761786fdf35ecdb84
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-14 09:43:07 -0500

    Use Data.Vector

--- haskell/day5.hs
+++ haskell/day5.hs
@@ -1,12 +1,14 @@
-walk :: Int -> Int -> [Int] -> Int
+import qualified Data.Vector as V
+
+walk :: Int -> Int -> V.Vector Int -> Int
 walk i a xs
     | i >= length xs = a
-    | otherwise = walk (i + xs !! i) (a + 1) (inc i xs)
+    | otherwise = walk (i + xs V.! i) (a + 1) (inc i xs)
 
-inc :: Int -> [Int] -> [Int]
-inc i xs = take i xs ++ [xs !! i + 1] ++ drop (i + 1) xs
+inc :: Int -> V.Vector Int -> V.Vector Int
+inc i xs = (V.take i xs) V.++ (V.fromList [xs V.! i + 1]) V.++ (V.drop (i + 1) xs)
 
 main = do
    content <- readFile "day5.txt"
-   let s = [read x :: Int | x <- lines content]
+   let s = V.fromList [read x :: Int | x <- lines content]
    print (walk 0 0 s)
