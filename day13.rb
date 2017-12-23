b6e0d819ef61a6cc548115c7c8d4e8b49be541dd
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-23 11:20:34 +0100

    Day 13 part II

--- day13.rb
+++ day13.rb
@@ -1,42 +1,56 @@
-State = Struct.new(:scanner_positions, :my_position) do
-  def caught?
-    scanner_positions.fetch(my_position, nil)&.peek == 0
-  end
-end
-
 def parse_line(s)
   s.split(':').map(&:strip).map(&:to_i)
 end
 
-def walk(h, state)
-  0.upto(h.keys.last).reduce(0) do |a, _i|
-    a += severity(h, state)
-    step!(state)
-    a
+def walk(h)
+  f = init_firewall(h)
+  0.upto(h.keys.last).reduce(0) do |a, i|
+    a + severity(f, i)
   end
 end
 
-def severity(h, state)
-  return 0 unless state.caught?
-  h[state.my_position] * state.my_position
+def severity(f, i)
+  positions = scanner_positions(f, i)
+  return 0 unless caught?(positions, i)
+  i * (f[i].max + 1)
 end
 
-def step!(state)
-  state.scanner_positions.values.each(&:next)
-  state.my_position += 1
-end
-
-def new_state(h)
-  State.new(h.map { |k, v| [k, cycle(v)] }.to_h, 0)
+def caught?(p, i)
+  p[i] == 0
 end
 
 def cycle(depth)
   up = 0.upto(depth - 1).to_a
   down = up.drop(1).reverse.drop(1)
-  up.concat(down).cycle
+  up.concat(down)
+end
+
+def safe?(f, delay)
+  0.upto(f.keys.last).detect { |i| caught?(scanner_positions(f, i + delay), i) }.nil?
+end
+
+def init_firewall(depths)
+  depths.map { |k, v| [k, cycle(v)] }.to_h
+end
+
+def scanner_positions(f, i)
+  f.map { |k, v| [k, v[i % v.length]] }.to_h
+end
+
+def part1(depths)
+  walk(depths)
+end
+
+# very slow, ~3min in my case
+def part2(depths)
+  f = init_firewall(depths)
+  0.upto(Float::INFINITY).detect do |i|
+    safe?(f, i)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   depths = File.readlines('day13.txt').map(&:strip).map(&method(:parse_line)).to_h
-  p walk(depths, new_state(depths))
+  p part1(depths)
+  p part2(depths)
 end
