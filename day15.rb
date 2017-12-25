33dc281b5f3a8efa62020cde3f1fad2ce0f91479
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-25 17:45:54 +0100

    Day 15 part II

--- day15.rb
+++ day15.rb
@@ -3,12 +3,29 @@ STARTING_VALUES = [591, 393].freeze
 FACTORS = [16_807, 48_271].freeze
 DIVISOR = 2_147_483_647
 N = 4 * 10**7
+M = 5 * 10**6
 
 def part1(starting_values, factors)
   starting_values.zip(factors)
                  .map { |a, b| generate_all(a, b) }
                  .transpose
                  .count { |a, b| match?(a, b) }
+end
+
+def part2(starting_values, factors, mods = [4, 8])
+  starting_values.zip(factors, mods)
+                 .map { |a, b, c| generate_all2(a, b, c) }
+                 .transpose
+                 .count { |a, b| match?(a, b) }
+end
+
+def generate_all2(v, factor, mod)
+  a = []
+  loop do
+    v = generate(v, factor)
+    a.push(v) if v % mod == 0
+    return a if a.length == M
+  end
 end
 
 def generate_all(starting_value, factor)
@@ -22,9 +39,10 @@ def generate(prev, factor)
 end
 
 def match?(a, b)
-  a % 2**16 == b % 2 **16
+  a % 2**16 == b % 2**16
 end
 
 if __FILE__ == $PROGRAM_NAME
   p part1(STARTING_VALUES, FACTORS)
+  p part2(STARTING_VALUES, FACTORS)
 end
