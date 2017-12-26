c6cbea77017e639820270a5b8d56a77b1d3bed9f
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-26 15:00:59 +0100

    Day 15 in Haskell

---
+++ haskell/day15.hs
@@ -0,0 +1,33 @@
+--startingValues = [65, 8921]
+startingValues = [591, 393]
+factors = [16807, 48271]
+d = [4, 8]
+divisor = 2147483647
+round2to16 = round $ 2**16
+numberOfPairs = round $ 4 * 10 ** 7
+numberOfPairs2 = round $ 5 * 10 ** 6
+
+part1 n = length [p | p <- take n pairs, match p]
+
+part2 n = length [p | p <- take n pairs2, match p]
+
+pairs = zip (xs !! 0) (xs !! 1)
+    where xs = map gen ys
+          ys = zip factors startingValues
+
+pairs2 = zip (xs !! 0) (xs !! 1)
+  where xs = map gen2 ys
+        ys = zip3 factors startingValues d
+
+gen (factor, startingValue) = iterate (next factor) startingValue
+
+gen2 (factor, startingValue, d) = filter (\x -> mod x d == 0) xs
+    where xs = gen (factor, startingValue)
+
+next factor x = mod (x * factor) divisor
+
+match (a, b) = mod a round2to16 == mod b round2to16
+
+main = do
+   print (part1 numberOfPairs)
+   print (part2 numberOfPairs2)
