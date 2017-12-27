a53c5fd7224508bb7461879a762b2b4f027e7d85
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-27 22:33:27 +0100

    Refactor a little bit and use Data.Vector.Unboxed

--- haskell/day5.hs
+++ haskell/day5.hs
@@ -1,25 +1,18 @@
-import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as V
 
 walk :: (Int -> V.Vector Int -> V.Vector Int) -> Int -> Int -> V.Vector Int -> Int
 walk inc i a xs
-    | i >= length xs = a
+    | i >= V.length xs = a
     | otherwise = walk (inc) (i + xs V.! i) (a + 1) (inc i xs)
 
 inc :: Int -> V.Vector Int -> V.Vector Int
-inc i xs = (V.take i xs) V.++ (V.singleton (xs V.! i + 1)) V.++ (V.drop (i + 1) xs)
-
---inc2 :: Int -> V.Vector Int -> V.Vector Int
---inc2 i xs
---    | step >= 3 = (V.take i xs) V.++ V.singleton (xs V.! i - 1) V.++ (V.drop (i + 1) xs)
---    | otherwise = (V.take i xs) V.++ V.singleton (xs V.! i + 1) V.++ (V.drop (i + 1) xs)
---    where step = xs V.! i
+inc i xs = V.update xs $ V.fromList [(i, xs V.! i + 1)]
 
 inc2 :: Int -> V.Vector Int -> V.Vector Int
 inc2 i xs
-    | step >= 3 = fst t V.++ V.singleton (xs V.! i - 1) V.++ V.tail (snd t)
-    | otherwise = fst t V.++ V.singleton (xs V.! i + 1) V.++ V.tail (snd t)
+    | step >= 3 = V.update xs $ V.fromList [(i, xs V.! i - 1)]
+    | otherwise = V.update xs $ V.fromList [(i, xs V.! i + 1)]
     where step = xs V.! i
-          t = V.splitAt i xs
 
 main = do
    content <- readFile "day5.txt"
