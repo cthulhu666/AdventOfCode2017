b244f06cc910f48c9f256c79285165fc9d1be7f9
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-27 23:43:12 +0100

    Get rid of Data.Text

--- haskell/day16.hs
+++ haskell/day16.hs
@@ -1,5 +1,6 @@
-import qualified Data.Text as T
 import Data.List
+import Data.List.Split (wordsBy)
+
 
 -- TODO: use more proper data structure, [(Char, Int)] perhaps?
 -- dancers = zip ['a'..'p'] [0..]
@@ -9,28 +10,26 @@ dance :: String -> [String -> String] -> String
 dance xs [] = xs
 dance xs (f:ys) = dance (f xs) ys
 
-parse :: T.Text -> String -> String
-parse s
+parse :: String -> String -> String
+parse (cmd:arg)
     | cmd == 's' = spin arg
     | cmd == 'x' = exchange arg
     | cmd == 'p' = partner arg
     | otherwise = fail "Unknown move"
-    where cmd = T.head s
-          arg = T.tail s
 
-spin :: T.Text -> String -> String
+spin :: String -> String -> String
 spin x xs =
     snd ys ++ fst ys
     where ys = splitAt n xs
           n = (length xs) - (atoi x)
 
-exchange :: T.Text -> String -> String
+exchange :: String -> String -> String
 exchange x xs = swap i j xs
-    where [i, j] = [atoi s | s <- T.split (=='/') x]
+    where [i, j] = [atoi s | s <- wordsBy (=='/') x]
 
-partner :: T.Text -> String -> String
+partner :: String -> String -> String
 partner p xs = swap i j xs
-    where [Just i, Just j] = [ elemIndex e xs | e <- [ T.head s | s <- T.split (=='/') p ]]
+    where [Just i, Just j] = [ elemIndex e xs | e <- [ head s | s <- wordsBy (=='/') p ]]
 
 -- https://stackoverflow.com/questions/30551033/swap-two-elements-in-a-list-by-its-indices
 swap f s xs = zipWith (\x y ->
@@ -38,10 +37,10 @@ swap f s xs = zipWith (\x y ->
     else if x == s then xs !! f
     else y) [0..] xs
 
-atoi :: T.Text -> Int
-atoi s = read . T.unpack $ s
+atoi :: String -> Int
+atoi s = read s
 
 main = do
    content <- readFile "day16.txt"
-   let moves = map parse . map T.strip . T.split (==',') . T.pack $ content
+   let moves = map parse . wordsBy (==',') $ content
    print $ dance dancers moves
