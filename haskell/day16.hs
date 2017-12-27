5f8f9e568fcb64b6451f2a07f941115f053ceb13
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-12-27 15:17:24 +0100

    Day 16 part I in Haskell

---
+++ haskell/day16.hs
@@ -0,0 +1,49 @@
+import qualified Data.Text as T
+import Data.Text.Read
+import Data.List
+
+-- TODO: use more proper data structure, [(Char, Int)] perhaps?
+-- dancers = zip ['a'..'p'] [0..]
+dancers = ['a'..'p']
+
+dance :: [([Char] -> [Char])] -> [Char] -> [Char]
+dance [] xs = xs
+dance moves xs = dance (tail moves) ys
+    where ys = head moves $ xs
+
+parse s
+    | cmd == 's' = spin arg
+    | cmd == 'x' = exchange arg
+    | cmd == 'p' = partner arg
+    | otherwise = fail "Unknown move"
+    where cmd = T.head s
+          arg = T.tail s
+
+spin :: T.Text -> [Char] -> [Char]
+spin x xs =
+    snd ys ++ fst ys
+    where ys = splitAt n xs
+          n = (length xs) - (atoi x)
+
+exchange :: T.Text -> [Char] -> [Char]
+exchange x xs = swap i j xs
+    where [i, j] = [atoi s | s <- T.split (=='/') x]
+
+partner :: T.Text -> [Char] -> [Char]
+partner p xs = swap i j xs
+    where [Just i, Just j] = [ elemIndex e xs | e <- [ T.head s | s <- T.split (=='/') p]]
+
+-- https://stackoverflow.com/questions/30551033/swap-two-elements-in-a-list-by-its-indices
+swap f s xs = zipWith (\x y ->
+    if x == f then xs !! s
+    else if x == s then xs !! f
+    else y) [0..] xs
+
+atoi :: Integral a => T.Text -> a
+atoi s = fst r
+    where (Right r) = decimal s
+
+main = do
+   content <- readFile "day16.txt"
+   let moves = map parse . map T.strip . T.split (==',') . T.pack $ content
+   print $ dance moves dancers
